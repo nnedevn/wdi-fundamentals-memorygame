049db28afb67628eac507efdce0a97870065a75e
Nick Nedev <nedevn@gmail.com>
2017-10-23 19:23:26 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,23 +1,45 @@
-/*jslint white:true*/
-/*global
-  alert
+/*jslint  browser:true, white:true  */
+
+/*global 
+  alert, console
 */
+
+
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+function checkForMatch() {
+  'use strict';
 
-console.log('User flipped queen');
-console.log('User flipped ' + cardsInPlay[1]);
+  if (cardsInPlay.length === 2) {
 
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert('You found a match!');
-  } else {
-    alert('Sorry, try again');
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert('You found a match!');
+    } else {
+      alert('Sorry, try again');
+    }
   }
 }
+
+function flipCard(cardId) {
+'use strict';
+  console.log('User flipped ' + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+}
+
+
+
+flipCard(2);
+flipCard(0);
+
+
+
+// var cardOne = cards[0];
+// var cardTwo = cards[2];
+// cardsInPlay.push(cardOne);
+// cardsInPlay.push(cardTwo);
+
+// console.log('User flipped queen');
+// console.log('User flipped ' + cardsInPlay[1]);
