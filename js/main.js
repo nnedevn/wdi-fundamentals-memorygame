272b48bbf3b195605cabae012dc98039e4d29591
Nick Nedev <nedevn@gmail.com>
2017-10-23 22:04:08 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -6,12 +6,27 @@
 
 
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [{
+  rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png"
+},{
+  rank: "queen",
+  suit: "diamonds",
+  cardImage: "images/queen-of-diamonds.png"
+},{
+  rank: "king",
+  suit: "hearts",
+  cardImage: "images/king-of-hearts.png"
+},{
+  rank: "king",
+  suit: "diamonds",
+  cardImage: "images/king-of-diamonds.png"
+}];
 var cardsInPlay = [];
 
 function checkForMatch() {
   'use strict';
-
   if (cardsInPlay.length === 2) {
 
     if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -22,10 +37,13 @@ function checkForMatch() {
   }
 }
 
+
 function flipCard(cardId) {
 'use strict';
-  console.log('User flipped ' + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log('User flipped ' + cards[cardId].rank);
+  console.log(cards[cardId].suit);
+  console.log(cards[cardId].cardImage);
+  cardsInPlay.push(cards[cardId].rank);
   checkForMatch();
 }
 
@@ -33,13 +51,3 @@ function flipCard(cardId) {
 
 flipCard(2);
 flipCard(0);
-
-
-
-// var cardOne = cards[0];
-// var cardTwo = cards[2];
-// cardsInPlay.push(cardOne);
-// cardsInPlay.push(cardTwo);
-
-// console.log('User flipped queen');
-// console.log('User flipped ' + cardsInPlay[1]);
